9b7fcffa0b2d26e908b9121e94a155a535f98f46
Piotr Furmanek <info.pete85@gmail.com>
2024-11-26 16:03:08 +0000

    selector implemented on breed details component

--- apps/breed-finder/src/app/components/breed-details/breed-details.component.ts
+++ apps/breed-finder/src/app/components/breed-details/breed-details.component.ts
@@ -1,62 +1,30 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { Breed } from '../../interfaces/breed.model';
 import { ActivatedRoute } from '@angular/router';
 import { getBreedDetails } from '../../state/breeds/breed.actions';
+import { selectBreedDetails } from '../../state/breeds/breed.selectors';
 
 @Component({
   selector: 'app-breed-details',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './breed-details.component.html',
-  styleUrl: './breed-details.component.scss',
+  styleUrls: ['./breed-details.component.scss'],
 })
-export class BreedDetailsComponent implements OnInit, OnDestroy {
+export class BreedDetailsComponent implements OnInit {
   private _store = inject(Store);
   private _route = inject(ActivatedRoute);
 
-  breedDetails$: Observable<Breed[] | null> = this._store.select(
-    (state) => state.breedState.breedDetails
-  );
-
-  breedData!: Breed;
-  selectedBreedName!: string;
-  subBreedData$!: Subscription;
-  subscriptionList = new Subscription();
+  breedDetails$: Observable<Breed[] | null> = this._store.select(selectBreedDetails);
 
   ngOnInit() {
-    this.selectedBreedName = this._route.snapshot.paramMap.get('name') ?? '';
+    const selectedBreedName = this._route.snapshot.paramMap.get('name') ?? '';
 
-    if (this.selectedBreedName) {
-      this._store.dispatch(
-        getBreedDetails({ selectedBreed: this.selectedBreedName })
-      );
+    if (selectedBreedName) {
+      this._store.dispatch(getBreedDetails({ selectedBreed: selectedBreedName }));
     }
-
-    this.getBreedDetails();
-  }
-
-  getBreedDetails() {
-    this.subBreedData$ = this.breedDetails$.subscribe({
-      next: (response) => {
-        this.subscriptionList.add(this.subBreedData$);
-        if (response) {
-          console.log(
-            'BreedDetailsComponent received breed details:',
-            response
-          );
-          this.breedData = response[0];
-        }
-      },
-      error: (err) => {
-        console.error(err);
-      },
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscriptionList.unsubscribe();
   }
 }
